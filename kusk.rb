fd467bc574f91720e98220261d4b1b9e1109ad7f
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-23 17:59:20 +0200

    Brew formula update for kusk version v1.0.5-rc1

--- kusk.rb
+++ kusk.rb
@@ -9,17 +9,17 @@ class Kusk < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_macOS_x86_64.tar.gz"
-      sha256 "1dd9246aad1468d125ed653945406e009d705b0c0a1ffe772d5f010e8e6b9798"
+    if Hardware::CPU.arm?
+      url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_macOS_arm64.tar.gz"
+      sha256 "08208d7f54bc1ae2dbdbfbba30974ac72f502afc3cf904c045a7af51788cd594"
 
       def install
         bin.install "kusk"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_macOS_arm64.tar.gz"
-      sha256 "e1979e4cd45aeab66fd0c24daecd0ac0275346f46963405f26d9a2ac01d32a67"
+    if Hardware::CPU.intel?
+      url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_macOS_x86_64.tar.gz"
+      sha256 "03e9a2c126eee9856db7bd370291b5c1b4de0382e26b5254dedfae3d83887661"
 
       def install
         bin.install "kusk"
@@ -30,7 +30,7 @@ class Kusk < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_Linux_arm64.tar.gz"
-      sha256 "a9a621edf299ca71d5cddd32c489dc39e1e398da48eb124e4eb064a130cb9e34"
+      sha256 "069b6fc28bb9274337244399b6911c771418d1d778384f354ea22c4d6e315e27"
 
       def install
         bin.install "kusk"
@@ -38,7 +38,7 @@ class Kusk < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/kubeshop/kusk/releases/download/v1.0.5-rc1/kusk_1.0.5-rc1_Linux_x86_64.tar.gz"
-      sha256 "bd9b04b9c3a92aec52cdd5aa7521d0e9edc865a7394b6d603617f269867ec16a"
+      sha256 "2a78d25191b3aed911be806709f9196c458a3b0630f800e9d9f4c823e5036658"
 
       def install
         bin.install "kusk"
